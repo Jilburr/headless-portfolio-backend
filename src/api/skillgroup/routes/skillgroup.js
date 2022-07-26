f277cec11ea51f271a0af28a5f2d5755deb04073
Jilbert <jilbert21@gmail.com>
2022-07-26 13:36:53 +0200

    Initial commit

---
+++ src/api/skillgroup/routes/skillgroup.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * skillgroup router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::skillgroup.skillgroup');
---
+++ src/api/skillgroup/services/skillgroup.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * skillgroup service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::skillgroup.skillgroup');
