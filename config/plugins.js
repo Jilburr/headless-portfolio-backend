2135c0387ad24275c1adf1ac43e7685ff6c9b78a
Jilbert <jilbert21@gmail.com>
2022-07-27 16:44:48 +0200

    added vercel info

--- config/plugins.js
+++ config/plugins.js
@@ -1,6 +1,12 @@
 module.exports = ({ env }) => ({
   "vercel-deploy": {
     enabled: true,
+    config: {
+      deployHook:
+        "https://api.vercel.com/v1/integrations/deploy/prj_qTabb3tfAgpno6YcVaUIydhhiA6L/xROW6TDJd5",
+      apiToken: "XPyRbZfYO20ZLasC3bVWJVY7",
+      appFilter: "headless-portfolio-frontend",
+    },
   },
   upload: {
     config: {
