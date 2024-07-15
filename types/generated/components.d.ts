b23f700e3a62f270d031a375e4dadc1a5c410a66
Jilbert <jilbert@keyagency.nl>
2024-07-15 16:16:39 +0200

    New lock file

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,18 +1,5 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
-export interface SharedSeo extends Schema.Component {
-  collectionName: 'components_shared_seos';
-  info: {
-    name: 'Seo';
-    icon: 'allergies';
-  };
-  attributes: {
-    metaTitle: Attribute.String & Attribute.Required;
-    metaDescription: Attribute.Text & Attribute.Required;
-    shareImage: Attribute.Media<'images'>;
-  };
-}
-
 export interface SectionsHero extends Schema.Component {
   collectionName: 'components_decoration_heroes';
   info: {
@@ -24,11 +11,24 @@ export interface SectionsHero extends Schema.Component {
   };
 }
 
+export interface SharedSeo extends Schema.Component {
+  collectionName: 'components_shared_seos';
+  info: {
+    name: 'Seo';
+    icon: 'allergies';
+  };
+  attributes: {
+    metaTitle: Attribute.String & Attribute.Required;
+    metaDescription: Attribute.Text & Attribute.Required;
+    shareImage: Attribute.Media;
+  };
+}
+
 declare module '@strapi/types' {
   export module Shared {
     export interface Components {
-      'shared.seo': SharedSeo;
       'sections.hero': SectionsHero;
+      'shared.seo': SharedSeo;
     }
   }
 }
